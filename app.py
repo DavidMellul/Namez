21fe5cba745bd36c02c52f1ad01014807e22bd41
David Mellul <David Mellul>
2017-04-18 22:46:25 +0200

    Everything has been made back from zero with Flask rather than Boottle.

--- app.py
+++ app.py
@@ -1,87 +1,107 @@
 import json
 from json import JSONDecodeError
-from bottle import request, response
-from bottle import post, get, put, delete
-from bottle import abort
-from bottle import run
+from flask import Flask, jsonify, request, abort
+
+app = Flask(__name__)
 
 
-# Insert a new element
-@post('/names')
+@app.route('/')
+def index_handler():
+    return "Welcome to Namez."
+
+
+# Insert a new name into the database
+@app.route('/names', methods=['POST'])
 def create_handler():
-    if not request_contains_json():
-        abort(400, "Bad request, please use well-formatted JSON")
+    # First, check if the request contains JSON
+    if request.json is None:
+        abort(400)
 
-    to_add = str(request.json['name']).upper()
+    # Then, look if the JSON content has got the key "name"
+    if request.json['name'] is None:
+        abort(400)
 
-    if to_add in names:
-        abort(409, "This name already exists.")
+    name = str(request.json['name']).upper()
 
-    names.append(to_add)
-    update_database_content()
+    # In the end, check if the name doesn't exist already
+    if name in names:
+        abort(409)
+
+    # Insert the new name
+    names.append(name)
+    save_data()
+    return jsonify({'name': name}), 201
 
 
-# List all the existing elements
-@get('/names')
+# Retrieve all the names from the database
+@app.route('/names', methods=['GET'])
 def read_handler():
-    response.headers['Content-Type'] = 'application/json'
-    data = json.dumps({'names': names})
-    return data
+    return jsonify({'names': names}), 200
 
 
-# Modify an existing element
-@put('/names/<name>')
-def update_handler(to_change):
-    to_change = str(to_change).upper()
+# Update an existing name
+@app.route('/names/<old_name>', methods=['PUT'])
+def update_handler(old_name):
+    old_name = str(old_name).upper()
 
-    if not request_contains_json():
-        abort(400, "Bad request, please use well-formatted JSON")
+    # First, check if the request contains JSON
+    if request.json is None:
+        abort(400)
 
-    new_one = str(request.json['name']).upper()
+    # Then, look if the JSON content has got the key "name"
+    if request.json['name'] is None:
+        abort(400)
 
-    if to_change not in names:
-        abort(404, "Name not found.")
+    new_name = str(request.json['name']).upper()
 
-    names[names.index(to_change)] = new_one
-    update_database_content()
+    # In the end, check if the name exists already
+    if old_name not in names:
+        abort(404)
+
+    # Update the old name with the new one
+    names[names.index(old_name)] = new_name
+    save_data()
+    return jsonify({'name': new_name}), 200
 
 
-# Delete an existing element
-@delete('/names/<name>')
+# Delete an existing name
+@app.route('/names/<name>', methods=['DELETE'])
 def delete_handler(name):
     name = str(name).upper()
-    if str(name).upper() in names:
-        names.remove(name)
-        update_database_content()
-    else:
-        abort(404, "Name not found.")
+    # First, check if the name exists in the database
+    if name not in names:
+        abort(404)
+
+    # Delete the requested name
+    names.remove(name)
+    save_data()
+    return jsonify({'name': name}), 204
 
 
-# Tells whether the request contains a JSON content
-def request_contains_json():
-    return request.headers['Content-Type'] == 'application/json'
-
-
-# Fill the file responsible for storing all the data. It's used the same way a .db file is used
-def update_database_content():
+# Make all the data persistent by storing it into data.json
+def save_data():
     try:
+        names.sort()
         with open('data.json', 'w') as out_file:
-            json.dump({'names': names}, out_file, indent=4)
+            json.dump({'names': names}, out_file)
     except FileNotFoundError:
         pass
 
 
-# This condition is useful so that if one imports this script, it won't run automatically
+# Do not start the server in case this script is imported into an other
 if __name__ == '__main__':
+    # List containing all the names
     names = list()
+    # List containing all the api keys required to communicate with the API
     keys = list()
 
-    # If it exists, load the file responsible for storing everything (database-like)
+    # If possible, retrieve what once has been stored
     try:
         with open('data.json', 'r') as in_file:
-            names = json.load(in_file)['names']
-            keys = json.load(in_file)['keys']
-    except (FileNotFoundError,JSONDecodeError):
+            names = sorted(json.load(in_file)['names'])
+            keys = sorted(json.load(in_file)['keys'])
+    except (FileNotFoundError, JSONDecodeError):
         pass
 
-    run(host="localhost", port=8080)
+    # Start the Flask server
+    app.run(host="127.0.0.1", port=8080)
